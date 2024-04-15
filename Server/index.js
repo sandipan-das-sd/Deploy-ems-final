6b9e59185513e2b2d18571c4fd94bab7eb0d5b8c
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-04-15 22:07:04 +0530

    server modified

--- Server/index.js
+++ Server/index.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 
 //Login and signup Role Based
 app.use(cors({
-    origin: 'https://server-ems-seven.vercel.app/',
+    origin: 'https://deploy-ems-final-4rxn.vercel.app/',
     methods: ["GET", "POST"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"]
