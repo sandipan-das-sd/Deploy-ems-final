4766462df6f538bf7fda46e200dcd52a2f2a137b
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-04-15 22:17:46 +0530

    index added

--- Server/index.js
+++ Server/index.js
@@ -36,7 +36,7 @@ app.use(cors({
 }));
 
 app.get('/', (req, res) => {
-    res.json({ "message": "Hello" });
+    res.json({ "message": "Hello server" });
 });
 
 app.post('/register', (req, res) => {
